ff96347b5e8248a3ed8509241b1323fd21285e18
LeonidBabkin <Babkinleo@gmail.com>
2022-11-06 12:56:47 +0300

    gendiff script modification

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,8 +9,13 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument(
+            '-f',
+            '--format',
+            action='store',
+            help='set format of output',
+            )
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
