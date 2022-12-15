f63af301a6bdf81e3b36613c8b5192e2b0310fc3
LeonidBabkin <Babkinleo@gmail.com>
2022-12-15 16:02:20 +0300

    adding test_plain.py

---
+++ tests/test_plain.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""JSON files diff test."""
+
+from gendiff.generate_diff import generate_diff
+import tests.expected as expected
+
+
+def test_plain():
+    result = generate_diff('./gendiff/fixtures/big1.json',
+                           './gendiff/fixtures/big2.json',
+                           'plain')
+    assert result == expected.PLAIN
