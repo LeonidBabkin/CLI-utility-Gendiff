9fc2b11898272f8de6bc15548018d8669e73be28
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2022-12-17 18:51:06 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -28,4 +28,30 @@ def test_small_json_yaml():
                            './tests/fixtures/file2.yaml',
                            'json')
     assert result == expected.SMALL_JSON
-  
+
+
+def test_big_plain_json():
+    result = generate_diff('./tests/fixtures/big1.json',
+                           './tests/fixtures/big2.json',
+                           'plain')
+    assert result == expected.BIG_PLAIN
+
+def test_small_plain_json():
+    result = generate_diff('./tests/fixtures/file1.json',
+                           './tests/fixtures/file2.json',
+                           'plain')
+    assert result == expected.SMALL_PLAIN
+
+
+def test_big_plain_yaml():
+    result = generate_diff('./tests/fixtures/big1.yaml',
+                           './tests/fixtures/big2.yaml',
+                           'plain')
+    assert result == expected.BIG_PLAIN
+
+
+def test_small_plain_yaml():
+    result = generate_diff('./tests/fixtures/file1.yaml',
+                           './tests/fixtures/file2.yaml',
+                           'plain')
+    assert result == expected.SMALL_PLAIN
