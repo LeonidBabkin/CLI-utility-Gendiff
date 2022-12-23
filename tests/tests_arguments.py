7866a85fd630d707c6c94c951a5fafbe58648484
LeonidBabkin <Babkinleo@gmail.com>
2022-12-23 15:48:04 +0300

    tests

--- tests/tests_arguments.py
+++ tests/tests_arguments.py
@@ -1,7 +1,8 @@
 import tests.expected as expected
 
-ARGS = [('file1.json', 'file2.json', 'json', 'expected.SMALL_JSON'),
-        ('big1.json', 'big2.json', 'json', 'expected.BIG_JSON'),
-        ('file1.yaml', 'file2.yaml', 'yaml', 'expected.SMALL_JSON'),
-        ('big1.yaml', 'big2.yaml', 'yaml', 'expected.BIG_JSON'),
+
+ARGS = [('file1.json', 'file2.json', 'json', expected.SMALL_JSON),
+        ('big1.json', 'big2.json', 'json', expected.BIG_JSON),
+        ('file1.yaml', 'file2.yaml', 'yaml', expected.SMALL_JSON),
+        ('big1.yaml', 'big2.yaml', 'yaml', expected.BIG_JSON),
         ]
