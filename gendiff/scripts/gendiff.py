4da3cc0e1e26223ffb4d3ef68e469e7451979164
LeonidBabkin <Babkinleo@gmail.com>
2022-12-24 16:13:10 +0300

    tweaking the script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,7 @@ import argparse
 from gendiff.gendiff import generate_diff
 
 
-def main():
+def parse_command_line():
     parser = argparse.ArgumentParser(description='Makes files discerned')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
@@ -21,4 +21,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+
+    parse_command_line()
