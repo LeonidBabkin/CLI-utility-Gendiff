c056562163ca6157cd19d33f8e0c3e56be8b3099
Leonid Babkin <112185390+LeonidBabkin@users.noreply.github.com>
2022-12-24 16:37:30 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,7 @@ import argparse
 from gendiff.gendiff import generate_diff
 
 
-def main():
+def parse_command_line():
     parser = argparse.ArgumentParser(description='Makes files discerned')
     parser.add_argument('first_file', metavar='first_file', type=str)
     parser.add_argument('second_file', metavar='second_file', type=str)
@@ -16,10 +16,13 @@ def main():
                         help='set format of output',
                         )
     args = parser.parse_args()
+
+
+def main():
+    args = parse_command_line()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     return print(diff)
 
 
 if __name__ == '__main__':
-
     main()
